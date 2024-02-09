bb8a549b8dc2bc1763ebeb7c16070148eeb7d293
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-02-09 17:23:11 +0100

    _header.sccs(Jumbo inside)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,33 @@
 <header>
-    <h1>Hello from Header</h1>
+    <div class="top-header">
+        <div class="a-container">
+            <a href="#">DC POWER VISA ® </a>
+            <a href="#">ADDITIONAL DC SITES ▼</a>
+        </div>
+    </div>
+    <div class="container">
+        <nav>
+            <!-- logo -->
+            <a href="#">
+                <img src="/images/dc-logo.png" alt="DC Logo">
+            </a>
+            
+            <!-- menu di navigazione -->
+            <ul>
+                @foreach($links as $link)
+                <li>
+                    <a href="{{ $link['url'] }}" class="{{$link['current'] ? 'active' : ''}}" >{{ $link['text'] }}</a>
+                </li>
+                @endforeach
+                
+            </ul>
+            <form>
+                <input type="text" placeholder="Search...">
+                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </nav>
+    </div>
 </header>
+<div class="jumbo">
+
+</div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>DC COMICS</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Section content dell'index!</h1>
 @endsection
