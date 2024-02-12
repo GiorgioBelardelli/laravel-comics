a22f039d3078a33b202cf00dc90a5cc231054fe5
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-02-12 11:07:00 +0100

    #

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,94 @@
 <footer>
-    <h1>Hello from footer</h1>
+    <section id="top-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a> </li>
+                        <li><a href="#">Movies</a> </li>
+                        <li><a href="#">TV</a> </li>
+                        <li><a href="#">Games</a> </li>
+                        <li><a href="#">Videos</a> </li>
+                        <li><a href="#">News</a> </li>
+                        <h3>SHOP</h3>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a> </li>
+                    </ul>
+                    <ul>
+                        <h3>DC</h3>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy(New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificate</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                    <ul>
+                        <h3>SITES</h3>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <img src="/images/dc-logo-bg.png" alt="DCicon">
+                </div>
+            </div>
+        </div>
+    </section>
+    <div id="bot-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <a href="#">
+                        SIGN-UP NOW!
+                    </a>
+                </div>
+                <div class="col">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                FOLLOW US
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <img src="/images/footer-facebook.png" alt="facebook.alt">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/footer-periscope.png" alt="periscope.alt">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/footer-twitter.png" alt="twitter.alt">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/footer-youtube.png" alt="youtube.alt">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/footer-pinterest.png" alt="pinterest.alt">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,5 +4,64 @@
     <title>DC COMICS</title>
 @endsection
 @section('content')
-    <h1>Section content dell'index!</h1>
+    <section id="comics-cards">
+        <div class="container">
+            <div class="current-series">
+                <h3>CURRENT SERIES</h3>
+            </div>
+            @foreach ($comics as $comic)
+            <div class="card">    
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="{{ $comic['thumb'] }}" alt="comic">
+                            <p>
+                                {{ $comic['title'] }}
+                            </p>    
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            @endforeach
+        </div>
+        <button class="load-button">
+            LOAD MORE
+        </button>
+    </section>
+    <section id="shop-nav">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="/images/buy-comics-digital-comics.png">
+                        <p>DIGITAL COMICS</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="/images/buy-comics-merchandise.png">
+                        <p>DC MERCHANDISE</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="/images/buy-comics-subscriptions.png">
+                        <p>SUBSCRIPTION</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="/images/buy-comics-shop-locator.png">
+                        <p>COMIC SHOP LOCATOR</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="/images/buy-dc-power-visa.svg">
+                        <p>DC POWER VISA</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
 @endsection
